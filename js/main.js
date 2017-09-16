bc9f8ffc4749bfa885d81dcf80548805e1175ab2
jruivo-dev <jruivo.dev@gmail.com>
2017-09-16 23:18:13 +0100

    Some design changes. Game is fully functional now.

--- js/main.js
+++ js/main.js
@@ -9,44 +9,95 @@ let currentPokemon = null;
 let pokeImage = document.querySelector('.poke-image');
 let passButton = document.querySelector('.pass-button')
 let guessButton = document.querySelector('.guess-button')
-let score = document.querySelector('.score');
-let lives = document.querySelector('.lives');
+let scoreElement = document.querySelector('.score');
+let livesElement = document.querySelector('.lives');
+let bestScoreElement = document.querySelector('.best-score');
+var snackbarElement = document.getElementById("snackbar")
 
 let gameOver = false;
 let score = 0;
 let lives = 3;
+let bestScore = 0;
 
 function init() {
     gameOver = false;
     score = 0;
     lives = 3;
+    updateStatsUI();
+    changePokemon();
 }
 
 
-
+// returns random pokemon image path
 function getRandomPokemon() {
     let index = Math.floor(Math.random() * pokeList.length - 1) + 1;
     currentPokemon = pokeList[index];
-
     return IMAGE_PATH + pokeList[index] + PNG;
 }
 
+// updates img element with new pokemon
 function changePokemon() {
     pokeImage.src = getRandomPokemon();
+    pokeImage.classList.add('desaturate')
 }
 
+//updates UI scores
+function updateStatsUI() {
+    bestScoreElement.innerHTML = bestScore;
+    livesElement.innerHTML = lives;
+    scoreElement.innerHTML = score;
+
+}
+
+// updates the stats after a guess
+function isGuessRight(guess) {
+    if (guess) {
+        score++;
+        if (score > bestScore) {
+            bestScore++;
+        }
+
+        snackbarElement.classList.add("show");
+        snackbarElement.innerHTML = 'Nice !'
+        snackbarElement.style.background = 'green';
+        // After 3 seconds, remove the show class from DIV
+        setTimeout(() => {
+            snackbarElement.classList.remove("show");
+            changePokemon();
+        }, 2200);
+
+    } else {
+        lives--;
+        snackbarElement.classList.add("show");
+        snackbarElement.innerHTML = 'Sorry, try again!'
+        snackbarElement.style.background = 'red';
+
+        if (lives === 0) {
+            snackbarElement.innerHTML = `Sorry , GAME OVER!`
+            init();
+
+        }
+        // After 3 seconds, remove the show class from DIV
+        setTimeout(() => {
+            snackbarElement.classList.remove("show");
+        }, 2200);
+    }
+    updateStatsUI()
+}
 
 
 
 function guessPokemon() {
     let guess = prompt(`What's your guess?`);
     if (guess.toLowerCase() === currentPokemon.toLowerCase()) {
-        alert('gotteeem');
-        changePokemon();
-    } else
-        alert(`That's a nope! Sorry`)
+        isGuessRight(true);
+        pokeImage.classList.remove('desaturate')
+    } else {
+        isGuessRight(false);
+    }
 }
 
 
 guessButton.addEventListener('click', guessPokemon);
-changePokemon();
+
+init();
